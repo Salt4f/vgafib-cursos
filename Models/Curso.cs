2d9c431467dbbd7ff08d40ff540e10486b2961c0
Salt4f <47352634+Salt4f@users.noreply.github.com>
2022-10-31 12:48:57 +0100

    Added database config

---
+++ Models/Curso.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VGAFIBCursos.Models
+{
+    public class Curso
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Image { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using VGAFIBCursos.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+    await context.Database.EnsureCreatedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
